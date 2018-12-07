6928ffb7cfe73b044324676317cb2eca1ce007ae
TheresaTan <theresatanubrata123@csu.fullerton.edu>
2018-12-07 14:48:25 -0800

    Updated to have more of the main functions

--- VectorString.h
+++ VectorString.h
@@ -1,5 +1,5 @@
 //
-// Created by Theresa Tanubrata on 12/7/18.
+// Header file that contains functions for all three parts
 //
 
 #ifndef VECTOR_STRING_INCLUDE
@@ -16,14 +16,25 @@
 //#define PAUSE cout << "\n\n"; system("pause");
 
 using namespace std;
-
+//PART I FUNCTIONS
 void readfile(string &); // read a file
 void remove_white_space(string &); // remove whitespace at the beginning of strings
 void remove_single_comments(string &); // remove single line comments
 void remove_multiple_comments(fstream&, string&); // remove multiple line comments
 void proper_spacing(string&); // add proper spacing to the line
-
 void program_vector(vector<string>&); // populate a vector with the tokens found in a program file
+void walk_stack(vector<string>&); //used to debug vectors
+
+
+//PART II FUNCTIONS
+bool check_file_for_errors(vector<string>&); //Check file for any automatic errors
+bool check_spelling(string&); //Check spelling for integer and word
+
+
+
+//PART III FUNCTION
+void create_cpp_file(vector<string>&); //Translate finalp2.txt file into c++ program
+
 
 
 // & in parameter to pass by reference and manipulate string::file_contents data
@@ -272,5 +283,260 @@ void program_vector(vector<string>& vector_given_tokens) {
         cout << x << ":\t" << vector_words[x] << "\n";
     }*/
 }
+
+
+
+
+void walk_stack(vector<string>& state_stack){
+    vector<string> temp = state_stack;
+    cout << "Current Stack: ";
+    for(auto it = temp.begin(); it < temp.end(); it++){
+        cout << *it << " \t";
+    }
+    cout << endl;
+}
+
+
+
+bool check_file_for_errors(vector<string>& vector_given_tokens){
+    //counters for the parentheses
+    int p_open = 0, p_close = 0;
+    //temporary vector string to keep track of declared variables
+    vector<string> temp;
+
+    //checks if program is in the file
+    if(vector_given_tokens[0] != "program"){
+        cout << endl;
+        cout << "program \tis expected\n";
+        return true;
+    }
+
+    //checks if end is in the file
+    if(vector_given_tokens[vector_given_tokens.size()-1] != "end"){
+        cout << endl;
+        cout << "end \tis expected\n";
+        return true;
+    }
+
+    //loops through the file to find any undefined variables, or illegal expressions
+    for(int i = 0; i < vector_given_tokens.size(); i++){
+        if(vector_given_tokens[i] == "("){
+
+            //counts left parentheses
+            p_open++;
+        }else if(vector_given_tokens[i] == ")"){
+
+            //counts right parentheses
+            p_close++;
+        }
+
+        if(vector_given_tokens[i] == "var"){
+
+            //looks for variables and pushes them on a temporary vector
+            //int j = i+1;
+            //increments to the beginning of the declaration list
+            i++;
+            bool dec_list_done = false;
+
+            //keeps looping through the declaration list until it reaches the end (: or integer)
+            while(!dec_list_done){
+
+                //if reading : or integer then the declaration list of variables is done
+                //put : and integer and ; in case one of them is missing from the file
+                if(vector_given_tokens[i] == ":" || vector_given_tokens[i] == "integer" || vector_given_tokens[i] == ";"){
+                    dec_list_done = true;
+                }
+
+                //pushes back only the variables and no punctuation marks
+                if(vector_given_tokens[i] != "," && vector_given_tokens[i] != ":") {
+                    temp.push_back(vector_given_tokens[i]);
+                }
+                i++;
+            }
+        }
+
+
+        //looping thorugh part that uses the declared variables: assigning values, showing values, and changing values
+        //Mainly looks for any illegal expressions or undefined variables being used
+        if(vector_given_tokens[i] == "begin" && !temp.empty()){
+            int k = i+1;
+            //current variable being looked at
+            string var_name;
+            //string used to check if the expression is legal
+            string check_expression;
+
+
+            while(vector_given_tokens[k] != "end"){
+                var_name = vector_given_tokens[k];
+                auto it = var_name.begin();
+                if( (*it >='a' && *it <= 'e')|| (*it >= '0' && *it <= '9') ){
+                    //specifically checks the variables
+                    if(*it >='a' && *it <= 'e'){
+                        bool defined = false;
+                        for(int l = 0; l < temp.size(); l++){
+                            if(temp[l] == var_name){
+                                defined = true;
+                            }
+                        }
+                        if(!defined){
+                            cout << endl;
+                            cout << var_name << "\tUndefined Expression!\n";
+                            return true;
+                        }
+                    }
+                    //checks whether expression is valid
+                    check_expression = vector_given_tokens[k+1];
+                    auto it2 = check_expression.begin();
+                    if( (*it2 >= 'a' && *it2 <= 'e') || *it2 == '(' || (*it2 >= '0' && *it2 <= '9')){
+                        cout << endl;
+                        cout << "Illegal Expression\n";
+                        return true;
+                    }
+
+
+                }
+                k++;
+
+            }
+        }
+
+
+    }
+
+    //checking if the left parentheses or right parentheses is more than the other
+    if(p_open < p_close){
+        cout << endl;
+        cout << "(\t Left Parentheses is missing.\n";
+        return true;
+    }else if(p_open > p_close){
+        cout << endl;
+        cout << ")\t Right Parentheses is missing.\n";
+        return true;
+    }
+
+    //returns false if there are no automatic errors in the file
+    return false;
+}
+
+//Only checks the spelling of the words integer and show
+bool check_spelling(string& word){
+    //checks to see if any of the letters in the referenced word contains any of the letters from the words "integer" or "show"
+    for(auto it = word.begin(); it != word.end(); ++it){
+        if(*it == 'i' || *it == 'n' || *it == 't' || *it == 'e' || *it == 'g' || *it == 'r'){
+            cout << endl;
+            cout << "Integer is misspelled!\n";
+            return true;
+        }else if(*it == 's' || *it == 'h' || *it == 'o' || *it == 'w'){
+            cout << endl;
+            cout << "Show is misspelled!\n";
+            return true;
+        }
+    }
+    return false;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//PART III FUNCTION
+
+//CREATING THE FILEP3.CPP
+void create_cpp_file(vector<string>& vector_given_tokens){
+    // Create .cpp file for output
+    fstream ifile3;
+    ifile3.open("filep3.cpp", ios::out);
+
+    // bool flag for tabs within "int main()"
+    bool tabFlag = false;
+
+    cout << "\nCreating new filep3.cpp\n\n";
+
+    // Begin iterating through vector
+    for (int y = 0; y < vector_given_tokens.size(); y++) {
+
+        // Rule for keyword "program"
+        if (vector_given_tokens[y] == "program") {
+            ifile3 << "#include <iostream>" << "\n"
+                   << "using namespace std;" << "\n";
+
+            while (vector_given_tokens[y] != ";")	// Increment vector position while ';' is not found
+                y++;
+        }
+
+
+            // Rule for keyword "var"
+        else if (vector_given_tokens[y] == "var") {
+            y += 1;	// Skip "var" token
+
+            do {
+                if (vector_given_tokens[y] == "integer")		// Shorten "integer" into "int".
+                    ifile3 << "int" << ' ';
+                else
+                    ifile3 << vector_given_tokens[y] << ' ';
+
+                y += 1;	// Increment vector position
+
+            } while (vector_given_tokens[y] != ";");	// Exit loop once a ';' is found
+
+            ifile3 << ";\n";	// Finish "var" rule by including this string
+
+        }
+
+
+            // Rule for keyword "begin"
+        else if (vector_given_tokens[y] == "begin") {
+            ifile3 << "int main()" << "\n"		// Ouput main function to .cpp
+                   << '{' << "\n\t";
+            tabFlag = true;		                // Begin tabbing each line
+        }
+
+
+            // Rule for "show"
+        else if (vector_given_tokens[y] == "show") {
+            ifile3 << "cout << ";
+
+            y += 2;		// Skip ahead two vector positions to bypass "show" and "("
+
+            ifile3 << vector_given_tokens[y];
+
+            y += 1;		// Skip ahead one vector position to bypass ")"
+
+            ifile3 << ' ';
+        }
+
+
+            // Rule for ';'
+        else if (vector_given_tokens[y] == ";" || vector_given_tokens[y] == "; ") {	// Question: Why do I need to check for "; "?
+            ifile3 << ";\n";
+
+            if (tabFlag == true && vector_given_tokens[y + 1] != "end")   	// tabFlag is true only after .cpp file has entered "int main() function.
+                ifile3 << "\t";						// Second condition used so the last "}" is NOT tabbed.
+        }
+
+
+            // Rule for "end"
+        else if (vector_given_tokens[y] == "end") {
+            tabFlag = false;		// Reset tabFlag to false
+            ifile3 << "}\n";		// Finish .cpp file
+        }
+
+
+        else
+            ifile3 << vector_given_tokens[y] << ' ';
+    }
+
+    cout << "File filep3.cpp has been created.\n";
+
+    ifile3.close();
+}
+
 #endif
 
